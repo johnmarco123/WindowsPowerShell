a87d1370d9281af06af23a65c2856a590b48b4df
johnmarco123 <johnmarcotasillo@gmail.com>
2024-06-14 10:42:09 -0400

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+$env:PATH += "C:\Users\John\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts"
+
+function jupyterlab() {
+  python3 -m jupyterlab
+}
